2684b97cf3ebc86ae6636f96a7e9437c8b47772e
sunwoo <ajtnlaos94@naver.com>
2020-09-24 12:35:41 +0900

    add : algo

--- codeup/Main_최소대금.java
+++ codeup/Main_최소대금.java
@@ -9,39 +9,24 @@ public class Main_최소대금 {
 	public static void main(String[] args) throws Exception {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		
 		int[] p = new int[3];
 		int[] j = new int[2];
 		
-		for(int i=0; i<3; i++) {
-			p[i] = Integer.parseInt(br.readLine());
-		}
-		
-		for(int i=0; i<2; i++) {
-			j[i] = Integer.parseInt(br.readLine());
+		for(int i=0; i<5; i++) {
+			if(i<3) p[i] = Integer.parseInt(br.readLine());
+			else j[i-3] = Integer.parseInt(br.readLine());
 		}
 		
 		Arrays.sort(p);
 		Arrays.sort(j);
 		
-		double temp  = (p[0]+j[0]) + ((p[0]+j[0])*0.1);
-		String result = temp+"";
-		int bol = 0;
-		for(int i=0; i<result.length(); i++) {
-			if(result.charAt(i)=='.') {
-				System.out.print(result.charAt(i));
-				bol = 1;
-				continue;
-			}
-			if(bol==0) {
-				System.out.print(result.charAt(i));
-			}
-			else {
-				System.out.print(result.charAt(i));
-				break;
-			}
-		}
-		System.out.println();
+		double temp = p[0]+j[0];
+		
+		String result = String.format("%.1f", temp*1.1);
+		
+		System.out.println(result);
+		
 	}
 
 }
